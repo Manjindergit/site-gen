88b4ad9d8f20d83ad30ef8b734407cfb99826d9a
Manjinderjit <manjindergrewal1997@gmail.com>
2026-03-23 16:05:50 -0700

    created pipeline for inline text

---
+++ src/functions/text_to_textnodes.py
@@ -0,0 +1,14 @@
+from src.textnode import TextType, TextNode
+import re
+from src.functions.splitnodes import split_nodes_delimiter, split_nodes_image, split_nodes_link
+
+def text_to_textnodes(text):
+    nodes = [TextNode(text, TextType.TEXT)]
+    nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
+    nodes = split_nodes_delimiter(nodes, "_", TextType.ITALIC)
+    nodes = split_nodes_delimiter(nodes, "`", TextType.CODE)
+    nodes = split_nodes_image(nodes)
+    nodes = split_nodes_link(nodes)
+   
+    return nodes
+            
